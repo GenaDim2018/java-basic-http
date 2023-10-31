7567461d73f296cc3732e4290fc38e9403fe2448
GenaDim2018 <genadim2018@gmail.com>
2023-10-31 11:56:31 +0300

    Init commit

--- server/src/main/java/ru/fisunov/http/server/MainApplication.java
+++ server/src/main/java/ru/fisunov/http/server/MainApplication.java
@@ -1,5 +1,8 @@
 package ru.fisunov.http.server;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -11,45 +14,45 @@ import java.util.concurrent.Executors;
 
 public class MainApplication {
     public static final int PORT = 8189;
-
-    // + К домашнему задания:
+    private static final Logger logger = LogManager.getLogger(MainApplication.class.getName());
+    // + К домашнему заданию:
     // Добавить логирование!!!
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
             Map<String, MyWebApplication> router = new HashMap<>();
             router.put("/calculator", new CalculatorWebApplication());
             router.put("/greetings", new GreetingsWebApplication());
-            System.out.println("Сервер запущен, порт: " + PORT);
+            logger.info("Сервер запущен, порт: " + PORT);
             ExecutorService executorService = Executors.newFixedThreadPool(10);
-            while(true) {
+            while (true) {
+                Socket socket = serverSocket.accept();
                 executorService.submit(() -> {
-                    try (Socket socket = serverSocket.accept()) {
-                        System.out.println("Клиент подключился");
+                    logger.info("Клиент подключился");
+                    try {
                         byte[] buffer = new byte[2048];
                         int n = socket.getInputStream().read(buffer);
                         String rawRequest = new String(buffer, 0, n);
                         Request request = new Request(rawRequest);
-                        System.out.println("Получен запрос:");
+                        logger.info("Получен запрос:");
                         request.show();
                         boolean executed = false;
                         for (Map.Entry<String, MyWebApplication> e : router.entrySet()) {
                             if (request.getUri().startsWith(e.getKey())) {
                                 e.getValue().execute(request, socket.getOutputStream());
                                 executed = true;
-                                break;
+                                socket.close();
                             }
                         }
                         if (!executed) {
                             socket.getOutputStream().write(("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<html><body><h1>Unknown application</h1></body></html>").getBytes(StandardCharsets.UTF_8));
+                            socket.close();
                         }
                     } catch (IOException e) {
-                        e.printStackTrace();
+                        logger.error("Ошибка ввода/вывода", e);
                     }
                 });
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 }
